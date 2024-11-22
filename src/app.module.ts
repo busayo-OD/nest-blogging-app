c91dd80f6492da6ca0e56adb609e7706a1f6498c
Busayo Dada <toyinoluwabusayo@gmail.com>
2024-11-22 19:53:43 +0100

    docker-compose and redis config

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,14 @@ import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { BlogModule } from './blog/blog.module';
 import { AuthModule } from './auth/auth.module';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { JwtGuard } from './auth/guards/jwt.guard';
 import { JwtStrategy } from './auth/strategy/jwt.strategy';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-yet';
+
+const DEFAULT_CACHE_TTL_SECONDS = 1000 * 60 * 60;
+const REDIS_HOST = 'localhost';
 
 @Module({
   imports: [
@@ -18,6 +23,18 @@ import { JwtStrategy } from './auth/strategy/jwt.strategy';
     }),
     UsersModule, 
     DatabaseModule, BlogModule, AuthModule,
+    CacheModule.registerAsync({
+      isGlobal: true,
+      useFactory: async () => ({
+        store: await redisStore({
+          ttl: DEFAULT_CACHE_TTL_SECONDS,
+          socket: {
+            host: REDIS_HOST,
+            port: 6379,
+          },
+        }),
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [
@@ -25,6 +42,10 @@ import { JwtStrategy } from './auth/strategy/jwt.strategy';
     {
       provide: APP_GUARD,
       useClass: JwtGuard,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
     },
     JwtStrategy,
   ],
